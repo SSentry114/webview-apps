206cfaadcc9ebb8835272368b19c2168a6d47415
User <user@S-Sentrys-MacBook-Pro.local>
2026-04-22 16:25:53 +0700

    a

---
+++ app373/fbsdk/com.app.moneytracking/lib/user/config.dart
@@ -0,0 +1,8 @@
+class AppConfig {
+  // Project folder name in Documents
+  static const String projectFolderName = 'project';
+
+  // App settings
+  // static const String appName = 'EspanolVocab';
+  static final String webViewUrl = 'https://terra-crit-15383593.figma.site';
+}
